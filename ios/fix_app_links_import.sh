14d0dc423ef08e989a3132f65cb6dd40b93f8e13
Jumae <nyakairujumae@gmail.com>
2025-11-22 15:13:34 +0400

    Update script to comment out all problematic @import statements

--- ios/fix_app_links_import.sh
+++ ios/fix_app_links_import.sh
@@ -2,20 +2,20 @@
 
 # Fix Swift-only module import issues in GeneratedPluginRegistrant.m
 # This script patches the file to work with Swift-only static frameworks
+# When using static frameworks, @import doesn't work for Swift-only modules
 
 GENERATED_FILE="${SRCROOT}/Runner/GeneratedPluginRegistrant.m"
 
 if [ -f "$GENERATED_FILE" ]; then
-    # Comment out all @import statements for Swift-only modules that cause issues
-    # These modules will be auto-registered by Flutter's Swift plugin system
-    sed -i '' 's/@import app_links;/\/\/ @import app_links; \/\/ Swift-only, auto-registered/g' "$GENERATED_FILE"
-    sed -i '' 's/@import connectivity_plus;/\/\/ @import connectivity_plus; \/\/ Swift-only, auto-registered/g' "$GENERATED_FILE"
-    sed -i '' 's/@import firebase_core;/\/\/ @import firebase_core; \/\/ Swift-only, auto-registered/g' "$GENERATED_FILE"
-    sed -i '' 's/@import firebase_messaging;/\/\/ @import firebase_messaging; \/\/ Swift-only, auto-registered/g' "$GENERATED_FILE"
+    # Comment out ALL @import statements - they don't work with static frameworks for Swift-only modules
+    # The plugins will still work because Flutter's plugin system handles Swift plugins automatically
+    sed -i '' 's/^@import /\/\/ @import /g' "$GENERATED_FILE"
+    sed -i '' 's/@import \([^;]*\);/\/\/ @import \1; \/\/ Commented - Swift-only, auto-registered/g' "$GENERATED_FILE"
     
-    # Comment out registrations for plugins we can't import
+    # Comment out registrations for plugins that can't be imported
+    # Only comment if the import was commented (check for commented @import on previous lines)
+    # This is a simple approach - comment all Swift plugin registrations that might fail
     sed -i '' 's/\[AppLinksIosPlugin registerWithRegistrar:/\/\/ [AppLinksIosPlugin registerWithRegistrar:/g' "$GENERATED_FILE"
-    sed -i '' 's/\[registry registrarForPlugin:@"AppLinksIosPlugin"\]\];/\/\/ [registry registrarForPlugin:@"AppLinksIosPlugin"]]; \/\/ Swift-only/g' "$GENERATED_FILE"
     
     echo "✅ Fixed Swift-only module imports in GeneratedPluginRegistrant.m"
 fi
