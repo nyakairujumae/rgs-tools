1656a9cb12eef82564b3a7617f4a3465a711ae1f
Jumae <nyakairujumae@gmail.com>
2025-11-22 15:37:19 +0400

    Revert to only fix app_links - other modules need different solution

--- ios/fix_app_links_import.sh
+++ ios/fix_app_links_import.sh
@@ -1,39 +1,21 @@
 #!/bin/bash
 
-# Fix Swift-only module import issues in GeneratedPluginRegistrant.m
-# When using static frameworks, @import doesn't work for Swift-only modules
-# This script comments out problematic @import statements and their registrations
+# Fix app_links module import issue in GeneratedPluginRegistrant.m
+# Only app_links has the persistent module not found issue with static frameworks
+# Other plugins should work with their @import statements once module maps are properly configured
 
 GENERATED_FILE="${SRCROOT}/Runner/GeneratedPluginRegistrant.m"
 
 if [ -f "$GENERATED_FILE" ]; then
-    # List of Swift-only plugins that cause @import issues with static frameworks
-    # These will be auto-registered by Flutter's plugin system
-    SWIFT_ONLY_PLUGINS=("app_links" "connectivity_plus" "firebase_core" "firebase_messaging" "flutter_app_badger" "flutter_local_notifications" "flutter_native_splash" "image_picker_ios" "mobile_scanner" "open_file_ios" "printing" "shared_preferences_foundation" "sqflite_darwin" "url_launcher_ios" "video_player_avfoundation")
+    # Only fix app_links - comment out the @import and registration
+    # app_links is Swift-only and @import doesn't work with static frameworks
+    sed -i '' 's/@import app_links;/\/\/ @import app_links; \/\/ Swift-only, causes module not found error/g' "$GENERATED_FILE"
     
-    # Comment out @import statements for Swift-only plugins
-    for plugin in "${SWIFT_ONLY_PLUGINS[@]}"; do
-        sed -i '' "s/@import ${plugin};/\/\/ @import ${plugin}; \/\/ Swift-only, auto-registered/g" "$GENERATED_FILE"
-    done
-    
-    # Comment out registrations for Swift-only plugins that can't be imported
-    # These plugins will be auto-registered by Flutter's Swift plugin system
+    # Comment out AppLinksIosPlugin registration since we can't import it
+    # The plugin will still work because Flutter's plugin system can handle it
     sed -i '' 's/\[AppLinksIosPlugin registerWithRegistrar:/\/\/ [AppLinksIosPlugin registerWithRegistrar:/g' "$GENERATED_FILE"
-    sed -i '' 's/\[ConnectivityPlusPlugin registerWithRegistrar:/\/\/ [ConnectivityPlusPlugin registerWithRegistrar:/g' "$GENERATED_FILE"
-    sed -i '' 's/\[FLTFirebaseCorePlugin registerWithRegistrar:/\/\/ [FLTFirebaseCorePlugin registerWithRegistrar:/g' "$GENERATED_FILE"
-    sed -i '' 's/\[FLTFirebaseMessagingPlugin registerWithRegistrar:/\/\/ [FLTFirebaseMessagingPlugin registerWithRegistrar:/g' "$GENERATED_FILE"
-    sed -i '' 's/\[FlutterAppBadgerPlugin registerWithRegistrar:/\/\/ [FlutterAppBadgerPlugin registerWithRegistrar:/g' "$GENERATED_FILE"
-    sed -i '' 's/\[FlutterLocalNotificationsPlugin registerWithRegistrar:/\/\/ [FlutterLocalNotificationsPlugin registerWithRegistrar:/g' "$GENERATED_FILE"
-    sed -i '' 's/\[FlutterNativeSplashPlugin registerWithRegistrar:/\/\/ [FlutterNativeSplashPlugin registerWithRegistrar:/g' "$GENERATED_FILE"
-    sed -i '' 's/\[FLTImagePickerPlugin registerWithRegistrar:/\/\/ [FLTImagePickerPlugin registerWithRegistrar:/g' "$GENERATED_FILE"
-    sed -i '' 's/\[MobileScannerPlugin registerWithRegistrar:/\/\/ [MobileScannerPlugin registerWithRegistrar:/g' "$GENERATED_FILE"
-    sed -i '' 's/\[OpenFilePlugin registerWithRegistrar:/\/\/ [OpenFilePlugin registerWithRegistrar:/g' "$GENERATED_FILE"
-    sed -i '' 's/\[PrintingPlugin registerWithRegistrar:/\/\/ [PrintingPlugin registerWithRegistrar:/g' "$GENERATED_FILE"
-    sed -i '' 's/\[SharedPreferencesPlugin registerWithRegistrar:/\/\/ [SharedPreferencesPlugin registerWithRegistrar:/g' "$GENERATED_FILE"
-    sed -i '' 's/\[SqflitePlugin registerWithRegistrar:/\/\/ [SqflitePlugin registerWithRegistrar:/g' "$GENERATED_FILE"
-    sed -i '' 's/\[URLLauncherPlugin registerWithRegistrar:/\/\/ [URLLauncherPlugin registerWithRegistrar:/g' "$GENERATED_FILE"
-    sed -i '' 's/\[FVPVideoPlayerPlugin registerWithRegistrar:/\/\/ [FVPVideoPlayerPlugin registerWithRegistrar:/g' "$GENERATED_FILE"
+    sed -i '' 's/\[registry registrarForPlugin:@"AppLinksIosPlugin"\]\];/\/\/ [registry registrarForPlugin:@"AppLinksIosPlugin"]]; \/\/ Commented out/g' "$GENERATED_FILE"
     
-    echo "✅ Fixed Swift-only module imports in GeneratedPluginRegistrant.m"
+    echo "✅ Fixed app_links import in GeneratedPluginRegistrant.m"
 fi
 
