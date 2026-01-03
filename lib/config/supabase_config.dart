fc485e259183ab6fa4996bc304934f4b98e07fc3
Jumae <nyakairujumae@gmail.com>
2026-01-03 21:32:31 +0400

    Remove web auth callback

--- lib/config/supabase_config.dart
+++ lib/config/supabase_config.dart
@@ -2,5 +2,4 @@ class SupabaseConfig {
   static const String url = 'https://npgwikkvtxebzwtpzwgx.supabase.co';
   static const String anonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5wZ3dpa2t2dHhlYnp3dHB6d2d4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA4NDgzOTAsImV4cCI6MjA3NjQyNDM5MH0.ucqQrk-5IcgF-wVLI2l1_CLYfu6ZDDrRuJ0Y3ugAaEU';
   static const String authCallbackUrl = 'com.rgs.app://auth/callback';
-  static const String webAuthCallbackUrl = 'https://rgstools.app/auth/callback';
 }
